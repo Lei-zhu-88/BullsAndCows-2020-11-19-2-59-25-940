90053a79be3c661f574350e1ece69a60e83c60fe
Lei ZHU <lzhu19@slb.com>
2020-11-19 16:47:59 +0800

    feat:can test with mock steup

--- BullsAndCowsTest/BullsAndCowsGameTest.cs
+++ BullsAndCowsTest/BullsAndCowsGameTest.cs
@@ -1,4 +1,5 @@
 using BullsAndCows;
+using Moq;
 using Xunit;
 
 namespace BullsAndCowsTest
@@ -42,15 +43,36 @@ namespace BullsAndCowsTest
             Assert.Equal(expect, actual);
         }
 
-        [Fact]
-        public void Should_return_0A4B_given_secret_1234_guess_4321()
+        [Theory]
+        [InlineData("4 3 2 1")]
+        [InlineData("3 4 2 1")]
+
+        public void Should_return_0A4B_given_secret_1234_guess_4321(string guess)
         {
             //given
             var secretGenerator = new TestSecretGenerator();
             var game = new BullsAndCowsGame(secretGenerator);
             var expect = "0A4B";
             //when
-            var actual = game.Guess("4 3 2 1");
+            var actual = game.Guess(guess);
+
+            //then
+            Assert.Equal(expect, actual);
+        }
+
+        [Theory]
+        [InlineData("1 2 3 4", "1234")]
+        [InlineData("5 6 7 8", "5678")]
+
+        public void Should_return_4A0B_When_All_digit_and_position_correct(string guess, string secret)
+        {
+            //given
+            var mockSecretGenerator = new Mock<TestSecretGenerator>();
+            mockSecretGenerator.Setup(mock => mock.GenerateSecret()).Returns(secret);
+            var game = new BullsAndCowsGame(mockSecretGenerator.Object);
+            var expect = "4A0B";
+            //when
+            var actual = game.Guess(guess);
 
             //then
             Assert.Equal(expect, actual);
