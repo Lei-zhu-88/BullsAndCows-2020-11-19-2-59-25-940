459f5a5c16910def68bbefe1be33659c71bf53b4
Lei ZHU <lzhu19@slb.com>
2020-11-19 15:55:00 +0800

    feat pass first unit test by hard code

--- BullsAndCowsTest/BullsAndCowsGameTest.cs
+++ BullsAndCowsTest/BullsAndCowsGameTest.cs
@@ -8,10 +8,32 @@ namespace BullsAndCowsTest
         [Fact]
         public void Should_create_BullsAndCowsGame()
         {
-            var secretGenerator = new SecretGenerator();
+            var secretGenerator = new TestSecretGenerator();
             var game = new BullsAndCowsGame(secretGenerator);
             Assert.NotNull(game);
             Assert.True(game.CanContinue);
+        }
+
+        [Fact]
+        public void Should_return_given_secret_1234_guess_5678()
+        {
+            //given
+            var secretGenerator = new TestSecretGenerator();
+            var game = new BullsAndCowsGame(secretGenerator);
+            var expect = "0A0B";
+            //when
+            var actual = game.Guess("5678");
+
+            //then
+            Assert.Equal(expect, actual);
+        }
+    }
+
+    public class TestSecretGenerator : SecretGenerator
+    {
+        public override string GenerateSecret()
+        {
+            return "1234";
         }
     }
 }
